c69730f2a0f54c24edf48a44ab00cf6ce79969f2
maheetha <maheetha.b@gmail.com>
2017-01-03 02:12:53 -0600

    Update Bootstrap.R

--- Bootstrap.R
+++ Bootstrap.R
@@ -1,91 +1,50 @@
 #!/srv/gs1/software/R/3.2.0/bin/Rscript
 
+## The purpose of this script is to take a SPF output, and develop a bootstrapped ratio of haplotype 1 / haplotype 2 ###
+library(data.table)
+library(plyr)
+
+
+#### First argument is the SPF, and the second argument is how many times you want the bootstrap function to sample ####
 args = commandArgs(trailingOnly = TRUE)
-table = read.table(args[1], header = T
-gene_list = table$V1
-gene_position = table$V2
+spf = read.table(args[1], header = T)
+bootstrap_interval = args[2]
 
-riboconfidenceintervals = c()
-rnaconfidenceintervals = c()
-significance = c()
-pvalues = c()
 
-names = seq(1, 100, 1)
-pvalue_het = c()
+### core bootstrap function: simple####
+bootstrap <- function(spf) {
 
-for (x in 1:length(table$V1)){
-	maternalribo = paste(gene_list[x], gene_position[x], "maternalRIBO", sep = ".")
-	paternalribo = paste(gene_list[x], gene_position[x], "paternalRIBO", sep = ".")
-	maternalrna = paste(gene_list[x], gene_position[x], "maternalRNA", sep = ".")
-	paternalrna = paste(gene_list[x], gene_position[x], "paternalRNA", sep = ".")
-
-	maternalribotable = read.table(maternalribo)
-	paternalribotable = read.table(paternalribo)
-	maternalrnatable = read.table(maternalrna)
-	paternalrnatable = read.table(paternalrna)
-
-	vectorribo = c()
-	vectorrna = c()
-	vectorOfPvalues = c()
-
-	for (o in 1:1000){
+	ratios = c()
 	
-	sampleribo = sample.int(length(maternalribotable[,3]), size = length(maternalribotable[,3]), replace = TRUE)
-	samplerna = sample.int(length(maternalrnatable[,3]), size = length(maternalrnatable[,3]), replace = TRUE)
+	### for a certain number of times ####
+	for (count in 1:bootstrap_interval){
 	
-	matribocounts = maternalribotable$V3[c(sampleribo)]
-	patribocounts = paternalribotable$V3[c(sampleribo)]
-	matrnacounts = maternalrnatable$V3[c(samplerna)]
-	patrnacounts = paternalrnatable$V3[c(samplerna)]
+		adjustment_count = length(mribo$REF_COUNT)
 
-	ratioribo = (sum(matribocounts) + length(maternalribotable[,3])) / (sum(matribocounts)+sum(patribocounts) + length(maternalribotable[,3]) + length(maternalribotable[,3]))
-	ratiorna = (sum(matrnacounts) + length(maternalrnatable[,3])) / (sum(matrnacounts)+sum(patrnacounts) + (2*length(maternalrnatable[,3])))
-	vectorribo = c(vectorribo, ratioribo)
-	vectorrna = c(vectorrna, ratiorna)
+		### randomly sample x number of positions
+		sampleribo = sample.int(adjustment_count, size = adjustment_count, replace = TRUE)
+
+		### sum up the counts
+		matribocounts = spf$REF_COUNT[c(sampleribo)]
+		patribocounts = spf$ALT_1_COUNT[c(sampleribo)]
+	
+		### ratio
+		ratio = (sum(matribocounts) + adjustment_count) / (sum(matribocounts)+ sum(patribocounts) + 2*adjustment_count)
+
+		### add to the vector of ratios
+		ratios = c(ratios, ratio)
+
+	}
+
+
+	return ratios
 
 	}
 	
-	meanribo = mean(vectorribo, na.rm = TRUE)
-	meanrna = mean(vectorrna, na.rm = TRUE)
-
-
-	ribolength = 30
-	rnalength = 75
-	upperboundribo = quantile(vectorribo, 0.99, na.rm = TRUE)
-	lowerboundribo = quantile(vectorribo, 0.01, na.rm = TRUE)
-	RIB = median(vectorribo)
-	upperboundrna = quantile(vectorrna, 0.99, na.rm = TRUE)
-	lowerboundrna = quantile(vectorrna, 0.01, na.rm = TRUE)
-	RNA = median(vectorrna)
-	print(upperboundribo)
-	print(lowerboundribo)
-	print(upperboundrna)
-	print(lowerboundrna)
-
-	riboCI = paste(lowerboundribo, upperboundribo, RIB, sep = "\t")
-	rnaCI = paste(lowerboundrna, upperboundrna, RNA, sep = "\t")
-	riboconfidenceintervals = c(riboconfidenceintervals, riboCI)
-	rnaconfidenceintervals = c(rnaconfidenceintervals, rnaCI)
-
-	if (	meanribo > meanrna){
-		if (lowerboundribo > upperboundrna){
-			significance = c(significance, "yes")		
-		} else {
-			significance = c(significance, "no")		
-		}
-	} else if (meanrna > meanribo){
-		if (lowerboundrna > upperboundribo){
-			significance = c(significance, "yes")		
-		} else {
-			significance = c(significance, "no")		
-		}
-	} else if (meanrna == meanribo){
-		significance = c(significance, "no")
-	}
-
-}
-
-finaltable = data.frame(gene_list, gene_position, riboconfidenceintervals, rnaconfidenceintervals, significance)
-
-tablename = paste(args[2], "FINAL_1000", sep = ".")
-write.table(finaltable, tablename, row.names = F, quote = F)
+#### get the lower bound and upper bound of a 99% confidence intreval and print
+vector_of_ratios = bootstrap(spf)
+upperbound = quantile(vectorribo, 0.99, na.rm = TRUE)
+lowerbound = quantile(vectorribo, 0.01, na.rm = TRUE)
+medianbound = median(vectorribo)
+CI = paste(lowerbound, upperbound, medianbound, sep = "\t")	
+print(CI)
